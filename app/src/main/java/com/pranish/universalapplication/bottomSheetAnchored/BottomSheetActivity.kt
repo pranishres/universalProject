fa1dfb05552c1d2df1554b6fbfeeff398d143899
Pranish Shrestha <pranish.shrestha@ekbana.info>
2019-08-28 14:15:50 +0545

    collapse/expand view only after bottomsheet has completed its animation

--- app/src/main/java/com/pranish/universalapplication/bottomSheetAnchored/BottomSheetActivity.kt
+++ app/src/main/java/com/pranish/universalapplication/bottomSheetAnchored/BottomSheetActivity.kt
@@ -1,6 +1,7 @@
 package com.pranish.universalapplication.bottomSheetAnchored
 
 import android.os.Bundle
+import android.view.View
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.google.android.material.bottomsheet.BottomSheetBehavior
@@ -8,19 +9,36 @@ import com.pranish.universalapplication.R
 import kotlinx.android.synthetic.main.activity_bottom_sheet.*
 import kotlinx.android.synthetic.main.layout_main_menu.*
 
+
 class BottomSheetActivity : AppCompatActivity() {
+    private var taped = false
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_bottom_sheet)
         fragmentMap.setOnClickListener {
+            taped = true
             Toast.makeText(this, "Clicked", Toast.LENGTH_SHORT).show()
             if (BottomSheetBehavior.from(nsvMainMenu).state == BottomSheetBehavior.STATE_COLLAPSED) {
                 BottomSheetBehavior.from(nsvMainMenu).state = BottomSheetBehavior.STATE_EXPANDED
-                SlideViewAnimation.expand(nsvMainMenuMain)
             } else {
                 BottomSheetBehavior.from(nsvMainMenu).state = BottomSheetBehavior.STATE_COLLAPSED
-                SlideViewAnimation.collapse(nsvMainMenuMain)
             }
         }
+
+        BottomSheetBehavior.from(nsvMainMenu).setBottomSheetCallback(object : BottomSheetBehavior.BottomSheetCallback() {
+            override fun onStateChanged(bottomSheet: View, newState: Int) {
+                if (taped) {
+                    if (newState == BottomSheetBehavior.STATE_COLLAPSED) {
+                        SlideViewAnimation.collapse(nsvMainMenuMain)
+                        taped = false
+                    } else if (newState == BottomSheetBehavior.STATE_EXPANDED) {
+                        SlideViewAnimation.expand(nsvMainMenuMain)
+                        taped = false
+                    }
+                }
+            }
+
+            override fun onSlide(bottomSheet: View, slideOffset: Float) {}
+        })
     }
 }
